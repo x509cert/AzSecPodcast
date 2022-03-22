af81ffdb1e1c35b2f4f8355e68eb44c5547a8819
Michael Howard (CYBER SECURITY) <mikehow@ntdev.microsoft.com>
2022-03-21 20:41:23 -0500

    updated ui.js - removed script tags

--- ui.js
+++ ui.js
@@ -1,32 +1,30 @@
-<script>
-      $(document).ready(function() {
+   $(document).ready(function() {
 
-      // collapse all but the first episode
-      $("div.EpisodeContent").hide();
-      $("div.EpisodeContent:first").show();
+   // collapse all but the first episode
+   $("div.EpisodeContent").hide();
+   $("div.EpisodeContent:first").show();
 
-      // set the correct expand/collapse icon
-      $("img.ColExpIcon").attr('src','images/expand.png');
-      $("img.ColExpIcon:first").attr('src','images/collapse.png');
+   // set the correct expand/collapse icon
+   $("img.ColExpIcon").attr('src','images/expand.png');
+   $("img.ColExpIcon:first").attr('src','images/collapse.png');
 
-      // toggle the cursor type when hovering over the episode header
-      $('h3.Heading').css('cursor', 'pointer'); 
+   // toggle the cursor type when hovering over the episode header
+   $('h3.Heading').css('cursor', 'pointer'); 
+   
+   // Handle click on a title - expand or collapse 
+   $("h3.Heading").click(function() {
+      const delay = 300;
       
-      // Handle click on a title - expand or collapse 
-      $("h3.Heading").click(function() {
-         const delay = 300;
-         
-         var h = "#Content-" + $(this).attr("id");
-         var ec = "ColExp-"+ $(this).attr("id");
+      var h = "#Content-" + $(this).attr("id");
+      var ec = "ColExp-"+ $(this).attr("id");
 
-         if ($(h).is(":hidden")) {
-            $(h).slideDown(delay);
-            document.getElementById(ec).src="images/Collapse.png";
-         }
-         else { 
-            $(h).slideUp(delay);
-            document.getElementById(ec).src="images/Expand.png";
-         }
-       })
-   });
-</script>
+      if ($(h).is(":hidden")) {
+         $(h).slideDown(delay);
+         document.getElementById(ec).src="images/Collapse.png";
+      }
+      else { 
+         $(h).slideUp(delay);
+         document.getElementById(ec).src="images/Expand.png";
+      }
+      })
+});
