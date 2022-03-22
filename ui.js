7754cff5cf2927809e701b7ca8f5df3650508457
Michael Howard (CYBER SECURITY) <mikehow@ntdev.microsoft.com>
2022-03-21 20:28:06 -0500

    moved inline code out to ui.js

---
+++ ui.js
@@ -0,0 +1,32 @@
+<script>
+      $(document).ready(function() {
+
+      // collapse all but the first episode
+      $("div.EpisodeContent").hide();
+      $("div.EpisodeContent:first").show();
+
+      // set the correct expand/collapse icon
+      $("img.ColExpIcon").attr('src','images/expand.png');
+      $("img.ColExpIcon:first").attr('src','images/collapse.png');
+
+      // toggle the cursor type when hovering over the episode header
+      $('h3.Heading').css('cursor', 'pointer'); 
+      
+      // Handle click on a title - expand or collapse 
+      $("h3.Heading").click(function() {
+         const delay = 300;
+         
+         var h = "#Content-" + $(this).attr("id");
+         var ec = "ColExp-"+ $(this).attr("id");
+
+         if ($(h).is(":hidden")) {
+            $(h).slideDown(delay);
+            document.getElementById(ec).src="images/Collapse.png";
+         }
+         else { 
+            $(h).slideUp(delay);
+            document.getElementById(ec).src="images/Expand.png";
+         }
+       })
+   });
+</script>
